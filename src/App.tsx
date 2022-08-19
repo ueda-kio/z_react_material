f9eb048ac2d0fd22594b8d67a8c08f97fbe8a650
上田貴央 <gibson511511@gmail.com>
2022-08-20 00:31:55 +0900

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+import './assets/css/App.css';
+
+const App: React.FC = () => {
+	const [count, setCount] = useState(0);
+
+	return <></>;
+};
+
+export default App;
