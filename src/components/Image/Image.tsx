f56766fe7ba97a11be847334fb61c9b48e2893dc
上田貴央 <gibson511511@gmail.com>
2022-09-04 00:15:30 +0900

    ImageList delete func add

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,9 +1,10 @@
+import React from 'react';
 import { css } from '@emotion/react';
 import Button from '@mui/material/Button';
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 import { styled } from '@mui/material';
-import React from 'react';
+import * as ComponentButton from '../../components/Button';
 
 const StyledButton = styled(Button)(({ theme }) => ({
 	height: theme.spacing(7),
@@ -24,6 +25,8 @@ const style = {
 		padding: 4px;
 		border: 1px dotted #616161;
 		border-radius: 8px;
+		background-color: #fff;
+		aspect-ratio: 1;
 	`,
 	inner: css`
 		position: relative;
@@ -59,6 +62,25 @@ const style = {
 		opacity: 0;
 		transition: opacity 0.15s;
 	`,
+	noSelect: css`
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+		gap: 4px;
+		width: 100%;
+		height: 100%;
+		border-radius: 8px;
+		background-color: #f8f8f8;
+	`,
+	noSelectText: css`
+		display: flex;
+		align-items: center;
+		gap: 4px;
+		font-weight: bold;
+		font-size: 0.9rem;
+		color: #8d8d8d;
+	`,
 };
 
 type ButtonProps = {
@@ -71,22 +93,35 @@ type Props = {
 	alt?: string;
 	changeButton: ButtonProps;
 	deleteButton: ButtonProps;
+	noSelect?: { isNoSelect: boolean; handleClick: React.MouseEventHandler<HTMLButtonElement> };
 };
 
-const Image: React.FC<Props> = ({ src, alt = '', changeButton, deleteButton }) => {
+const Image: React.FC<Props> = ({ src, alt = '', changeButton, deleteButton, noSelect }) => {
 	return (
 		<div css={style.wrapper}>
-			<div css={style.inner}>
-				<img css={style.img} src={src} alt={alt} />
-				<div css={style.buttonWrap} className="hover-target">
-					<StyledButton variant="outlined" startIcon={<AddPhotoAlternateIcon />} onClick={changeButton.onClick}>
-						{changeButton.text}
-					</StyledButton>
-					<StyledButton variant="outlined" startIcon={<DeleteOutlineIcon />} onClick={deleteButton.onClick}>
-						{deleteButton.text}
-					</StyledButton>
+			{noSelect?.isNoSelect ? (
+				<div css={style.noSelect}>
+					<span css={style.noSelectText}>
+						<AddPhotoAlternateIcon />
+						フェア画像
+					</span>
+					<ComponentButton.Secondary size="small" sx={{ fontSize: '0.75rem', padding: '0 20px' }} onClick={noSelect.handleClick}>
+						フォトアルバムから選択
+					</ComponentButton.Secondary>
 				</div>
-			</div>
+			) : (
+				<div css={style.inner}>
+					<img css={style.img} src={src} alt={alt} />
+					<div css={style.buttonWrap} className="hover-target">
+						<StyledButton variant="outlined" startIcon={<AddPhotoAlternateIcon />} onClick={changeButton.onClick}>
+							{changeButton.text}
+						</StyledButton>
+						<StyledButton variant="outlined" startIcon={<DeleteOutlineIcon />} onClick={deleteButton.onClick}>
+							{deleteButton.text}
+						</StyledButton>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 };
--- src/template/FairContent/Images.tsx
+++ src/template/FairContent/Images.tsx
@@ -12,7 +12,8 @@ const style = {
 		border-top: ${utils.border};
 	`,
 	list: css`
-		display: flex;
+		display: grid;
+		grid-template-columns: 1fr 1fr 1fr;
 		gap: 12px;
 	`,
 	listItem: css`
@@ -25,35 +26,64 @@ const style = {
 	`,
 };
 
-const handleChange: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-	console.log(e);
-};
-const handleDelete: React.MouseEventHandler<HTMLButtonElement> = (e) => {
-	console.log(e);
-};
-
 const Images = () => {
 	const [images, setImages] = useState([
 		{ id: nanoid(), src: 'https://placehold.jp/3d4070/ffffff/150x150.png?text=img1' },
 		{ id: nanoid(), src: 'https://placehold.jp/3d4070/ffffff/150x150.png?text=img2' },
 		{ id: nanoid(), src: 'https://placehold.jp/3d4070/ffffff/150x150.png?text=img3' },
 	]);
-	console.log(images);
+
+	const handleChange: React.MouseEventHandler<HTMLButtonElement> = (e) => {
+		console.log(e);
+	};
+	const handleDelete = (id: string) => {
+		setImages((v) => v.filter((image) => image.id !== id));
+	};
+	const addPhoto = () => {
+		setImages((v) => {
+			return [...v, { id: nanoid(), src: 'https://placehold.jp/3d4070/ffffff/150x150.png?text=add' }];
+		});
+	};
 
 	return (
 		<div css={style.wrapper}>
 			<Reorder.Group as="ul" axis="x" onReorder={setImages} values={images} css={style.list}>
 				<AnimatePresence initial={false}>
 					{images.map((image) => (
-						<Reorder.Item value={image} id={image.id} key={image.id} css={style.listItem}>
+						<Reorder.Item
+							initial={{ opacity: 0 }}
+							animate={{ opacity: 1 }}
+							exit={{ opacity: 0 }}
+							value={image}
+							id={image.id}
+							key={image.id}
+							css={style.listItem}
+						>
 							<Image
 								src={image.src}
 								changeButton={{ text: '変更', onClick: handleChange }}
-								deleteButton={{ text: '削除', onClick: handleDelete }}
+								deleteButton={{ text: '削除', onClick: () => handleDelete(image.id) }}
 							/>
 						</Reorder.Item>
 					))}
 				</AnimatePresence>
+				{images.length < 3 ? (
+					<motion.li
+						initial={{ opacity: 0, display: 'none' }}
+						animate={{ opacity: 1, display: 'initial' }}
+						exit={{ opacity: 0 }}
+						transition={{ delay: 0.3 }}
+					>
+						<Image
+							src="https://placehold.jp/3d4070/ffffff/150x150.png?text=add"
+							changeButton={{ text: '変更', onClick: () => {} }}
+							deleteButton={{ text: '削除', onClick: () => {} }}
+							noSelect={{ isNoSelect: true, handleClick: addPhoto }}
+						/>
+					</motion.li>
+				) : (
+					false
+				)}
 			</Reorder.Group>
 		</div>
 	);
