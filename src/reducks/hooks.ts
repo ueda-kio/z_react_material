990ea6611ce683cc815b4ccd145aaca1cfb11163
上田貴央 <gibson511511@gmail.com>
2022-09-07 00:14:55 +0900

    custom hooks add

---
+++ src/reducks/hooks.ts
@@ -0,0 +1,31 @@
+import { useDispatch, useSelector } from 'react-redux';
+import type { RootState, AppDispatch } from './store/store';
+import type { TypedUseSelectorHook } from 'react-redux';
+
+export const useAppDispatch: () => AppDispatch = useDispatch;
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
+
+export const useFairSelector = () => {
+	const fair = useAppSelector((state: RootState) => state.fair);
+	return { fair };
+};
+
+export const useUnitSelector = () => {
+	const unit = useAppSelector((state: RootState) => state.receptionUnit);
+	return { unit };
+};
+
+export const useConditionSelector = () => {
+	const condition = useAppSelector((state: RootState) => state.condition);
+	return { condition };
+};
+
+export const useReceptionUnitSelector = () => {
+	const receptionUnit = useAppSelector((state: RootState) => state.receptionUnit);
+	return { receptionUnit };
+};
+
+export const useReservationSelector = () => {
+	const reservation = useAppSelector((state: RootState) => state.reservation);
+	return { reservation };
+};
